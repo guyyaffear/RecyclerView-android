b29e06e12e61b942332d1751370eccd3484833e4
GuyYaffeErmoza <guyyaffear@gmail.com>
2020-05-26 22:51:37 -0400

    adding func

---
+++ Projects/app/src/main/java/com/example/d_stage/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.d_stage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val pointer ="https://en.wikipedia.org/wiki/Kotlin_(programming_language)#/media/File:Kotlin-logo.svg"
+    }
+}
