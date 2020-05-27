26022e2e9446e0c84af26fd93e765b887e37d6ee
GuyYaffeErmoza <guyyaffear@gmail.com>
2020-05-26 23:05:40 -0400

    finish with the main file

--- Projects/app/src/main/java/com/example/d_stage/MainActivity.kt
+++ Projects/app/src/main/java/com/example/d_stage/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.d_stage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var blogAdapter: BlogRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,6 +14,13 @@ class MainActivity : AppCompatActivity() {
         val pointer ="https://en.wikipedia.org/wiki/Kotlin_(programming_language)#/media/File:Kotlin-logo.svg"
     }
     private fun initRecyclerView(){
-        rec
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val topSpacingDecorator = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingDecorator)
+            blogAdapter = BlogRecyclerAdapter()
+            adapter = blogAdapter
+        }
+
     }
 }
